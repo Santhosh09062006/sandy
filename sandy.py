85267171038c7d6e91c12fcf7163c299787ba2f6
Santhosh09062006 <sandy09062006@gmail.com>
2024-11-17 22:09:40 +0530

    first commit

---
+++ sandy.py
@@ -0,0 +1,45 @@
+import streamlit as st
+
+st.title("binary search number guessing game")
+
+st.write("think of a number between 1 and 100, and i will try to guess it !")
+st.write("you will tell me if my guess is too high,too low or correct.")
+ 
+ if 'low'not st.session_state:
+    st.session_state.low=1
+    st.ession_state.high=100
+    st.session_state.guess=(st.session_state.low+st.session_state.high)//2
+    st.session_state.attempts=0
+
+st.write(f"my current gues is:**{st.session_state}**")
+
+col1,col2,col3=st.columns(3)
+
+with col1:
+    if st.button("too low"):
+        st.session_state.low=st.session_state.attempts+=
+
+with col2:
+    if st.button("too high"):
+        st.session_state=st.session_state.guess-1
+        st.session_state.attempts+=1
+
+with col3:
+    if st.button("correct!"):
+        st.success(f"i guessed your number**{st.session_state.guess}**{st.session_state.attempts}attempts!")
+        st.session_state.low=1
+        st.session_state.high=100
+        st.session_state.attempts=0
+
+if st.session_state.low<=st.session_state.high:
+    st.session_state.guess=(st.session_state.low+st.session_state.high)//2
+
+else:
+    st.error("it seems like there was a mistake. please restart the game.")
+
+if st.button("reset game"):
+    st.session_state.low=1
+    st.session_state.high=100
+    st.session_state.guess=(st.session_stae.low+st.session_state.high)//2
+    st.session_state.attempts=0
+    st.success("game has been reset!start thinking of a new number.")
