0ce872894272a4a15af6651f5abcaa08b54a7ca6
Santhosh09062006 <sandy09062006@gmail.com>
2024-11-18 15:18:43 +0530

    Update sandy.py

--- sandy.py
+++ sandy.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 port=st.button("portfolio")
 if port:
-   st.title("PORTFOLIO for Thanush AS")
+   st.title("PORTFOLIO for Nithin k9")
    st.subheader("technical skills : python,html and cs")
    st.subheader("electronic communication concepts:semiconductor circuits")
 
